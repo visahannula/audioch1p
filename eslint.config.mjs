0ecbc9c1b731e9ef9b931ddfb4f95308828db7e9
Visa Hannula <visa@viha.fi>
2024-07-09 01:40:17 +0300

    Eslint React plugin add and bump version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactpkg from "eslint-plugin-react";
+//import pluginReactConfig from "eslint-plugin-react/configs/recommended";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: 
+    { parserOptions: { ecmaFeatures: { jsx: true } } },
+    globals: globals.browser
+  },
   pluginJs.configs.recommended,
+  reactpkg.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  //...fixupConfigRules(activeRulesConfig)
 ];
